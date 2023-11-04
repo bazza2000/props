fb5895e5c3003969c0e497f7e564bf7ff9faaaf8
bazza2000 <bazza2000@users.noreply.github.com>
2023-11-04 12:51:28 +0000

    Update Build.groovy

--- job/Build.groovy
+++ job/Build.groovy
@@ -30,10 +30,14 @@ def GenerateJob(def Team, def App, def env_name, def job_name) {
         wrappers {
             timestamps()
         }
+        environment {
+          TF_VER = sh(script: 'cat .terraform-version', , returnStdout: true).trim()
+        }
         steps {
             environmentVariables {
                  propertiesFile('.terraform_version')
             }
+            echo "PATH=${TF_VER}"
         }
         publishers {
             git {
